5a6051a79d29af617c2c506b55c6670a4897f0f8
Per Hejndorf <perh@seges.dk>
2022-04-10 08:05:59 +0200

    Add project files.

---
+++ AuditReader/Audit.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Audit
+{
+    public long ID { get; set; } // bigint, not null
+    public DateTime? Updatetime { get; set; } // datetime, null
+    public string? UserName { get; set; } // varchar(50), null
+    public string? TableName { get; set; } // varchar(50), null
+    public int? DeleteCount { get; set; } // int, null
+    public string? DeleteXML { get; set; } // XML(.), null
+    public int? InsertCount { get; set; } // int, null
+    public string? InsertXML { get; set; } // XML(.), null
+}
---
+++ AuditReader/Program.cs
@@ -0,0 +1,39 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.IO.Compression;
+using System.Text.Json;
+
+var config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+
+var connectionstring = config["ConnectionString"];
+
+using (var con = new SqlConnection(connectionstring))
+{
+    con.Open();
+
+    // Dapper 
+    Console.WriteLine("Fetch data");
+
+    var audits = await con.QueryAsync<Audit>("SELECT TOP 20 * FROM Audit ORDER BY Id DESC;");
+
+    foreach (var audit in audits)
+    {
+        using (var memorystream = new MemoryStream())
+        {
+            // Data-object (byte array) to transmit
+            await JsonSerializer.SerializeAsync(memorystream, audit);
+            var compressedbytes = memorystream.GZipBytes(CompressionMode.Compress);
+            
+            // Print
+            var percent = compressedbytes.Length * 100 / memorystream.Length;
+            Console.WriteLine($"{audit.Updatetime} {memorystream.Length,10:#} {compressedbytes.Length,10:#} {percent,4:#}%");
+
+            // Unpack received data
+            var decompressedbytes = compressedbytes.GZipBytes(CompressionMode.Decompress);
+            var x = JsonSerializer.Deserialize<Audit>(decompressedbytes);
+        }
+    }
+}
+
+Console.WriteLine("Finished..");
